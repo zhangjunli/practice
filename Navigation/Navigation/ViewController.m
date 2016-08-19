bf39336a42efa8487c12ce9ab6020c7f59776621
zhangjunli <2501094370@qq.com>
2016-08-19 13:17:19 +0800

    两种方式 在a界面隐藏导航条，在b界面出现导航条
    在简书上看到的方法，挺好用的。写到demo中，可以随时看到

--- Navigation/Navigation/ViewController.m
+++ Navigation/Navigation/ViewController.m
@@ -9,14 +9,39 @@
 #import "ViewController.h"
 #import "FirstViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UINavigationControllerDelegate>
 
 @end
 
 @implementation ViewController
+/*
+//第一种方式隐藏导航栏。瑕疵是，在有tabbar存在时，点击tabbar 切换视图时，会看到navigationbar正在隐藏的动画
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+}
+ */
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.navigationController.delegate = self;
+}
+
+//第二种方式，隐藏导航栏。不存在第一种方式的瑕疵
+- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated{
+    
+    // 判断要显示的控制器是否是自己
+    BOOL isShowHomePage = [viewController isKindOfClass:[self class]];
+    
+    [self.navigationController setNavigationBarHidden:isShowHomePage animated:YES];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
