fc228c317b957e09890382daf4f734cef586f0e7
zhangjunli <2501094370@qq.com>
2016-10-25 19:22:48 +0800

    去除cell的选中色

--- Project/Project/ViewController.m
+++ Project/Project/ViewController.m
@@ -43,6 +43,10 @@
     return cell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
